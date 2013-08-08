be8220caafd77b1ba073a3fbbeb4e0c4237ecaa4
frob <frob@249517.no-reply.drupal.org>
2013-08-07 23:13:08 -0700

    worked out a couple of bugs

--- assert.js
+++ assert.js
@@ -7,14 +7,18 @@
  */
 (function( window, undefined ) {
 
-  var assert = function( outcome, description, log) {
-    // set a default value for log parameter
-    a = typeof a !== 'undefined' ? a : true;
+  var assert = function( outcome, description, flag, customLog) {
+    // set a default value for parameters
+    flag = typeof flag !== 'undefined' ? flag : 'CONSOLE';
+    customLog = typeof customLog !== 'undefined' ? customLog : 'CONSOLE';
+
+    // Abstract out the console.log to allow for log extensibilty.
+    var log = console.log;
 
     var status = (outcome) ? "[PASS]" : "[FAIL]";
 
-    if(log) {
-      console.log(status + " | " description);
+    if(flag == 'CONSOLE') {
+      log(status + " | " + description);
     }
 
     if(status == "[PASS]") {
